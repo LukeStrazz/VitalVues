211af36e97bf9c09c049e2ae8b3070b833a924bf
luke-angelostrazzera <lukestrazzera@gmail.com>
2024-08-27 11:24:52 -0400

    Initial Commit

---
+++ AiDietPlan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AiDietPlan.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
