5098e35fba8c20b241250e301bd80c2554752e79
Luke <lukestrazzera@gmail.com>
2024-09-11 20:05:58 -0400

    Migration for target weight

---
+++ VVData/Migrations/20240912000118_TargetWeightForGoals.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TargetWeightForGoals : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "targetWeight",
+                table: "Goals",
+                type: "float",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "targetWeight",
+                table: "Goals");
+        }
+    }
+}
