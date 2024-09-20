75e631c3c10534579114a9544d42683d364b1ce3
Emely <emelyy9870@gmail.com>
2024-09-20 18:11:43 -0400

    Timer saves time in database

--- VVServices/ViewModels/FastingViewModel.cs
+++ VVServices/ViewModels/FastingViewModel.cs
@@ -1,18 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Services.ViewModels;
-
-public class FastingViewModel
+namespace Services.ViewModels
 {
-    public int Hours { get; set; }
-    public int Minutes { get; set; }
-    public int Seconds { get; set; }
-    public string userID { get; set; }
-    public DateTime start { get; set; }
-    public DateTime end { get; set; }
+    public class FastingViewModel
+    {
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+        public string userID { get; set; }
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
 
+    }
 }
