757632aee32f627ef4e3ec0599b52775a046d7c5
Luke <lukestrazzera@gmail.com>
2024-10-01 12:30:46 -0400

    Cleanup p2

--- VitalVues/Controllers/LetsChatController.cs
+++ VitalVues/Controllers/LetsChatController.cs
@@ -68,40 +68,6 @@ public class LetsChatController : Controller
         return Json(new { success = false, message = response });
     }
 
-    //[HttpPost]
-    //[Route("GetChatResponse")]
-    //public async Task<IActionResult> GetChatResponse([FromBody] ChatRequest request)
-    //{
-    //    var client = _clientFactory.CreateClient();
-    //    var apiKey = _configuration["API_KEY"];
-    //    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
-
-    //    var payload = new
-    //    {
-    //        model = "gpt-4o-mini",
-    //        messages = request.Messages.Select(m => new { role = m.Role, content = m.Content }).ToList()
-    //    };
-
-    //    var requestMessage = new HttpRequestMessage(HttpMethod.Post, "https://api.openai.com/v1/chat/completions")
-    //    {
-    //        Content = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json")
-    //    };
-
-    //    var response = await client.SendAsync(requestMessage);
-    //    if (response.IsSuccessStatusCode)
-    //    {
-    //        var responseContent = await response.Content.ReadAsStringAsync();
-    //        var jsonResponse = JObject.Parse(responseContent);
-    //        var reply = jsonResponse["choices"][0]["message"]["content"].ToString();
-    //        return Json(new { message = reply });
-    //    }
-    //    else
-    //    {
-    //        var errorContent = await response.Content.ReadAsStringAsync();
-    //        return StatusCode((int)response.StatusCode, "Error from API: " + errorContent);
-    //    }
-    //}
-
     [HttpPost]
     [Route("GetChatResponse")]
     public async Task<IActionResult> GetChatResponse([FromBody] ChatRequest request)
