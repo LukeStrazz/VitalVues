352d73c260d7166b59e049424e8b2cebbc4dc54a
JayWhite17 <whitj13@farmingadle.edu>
2024-10-08 17:19:49 -0400

    Completed about us page

---
+++ VitalVues/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VitalVues.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
