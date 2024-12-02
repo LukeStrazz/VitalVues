eef97a48a76257012fb91c47703be699be2727c9
Emely <emelyy9870@gmail.com>
2024-12-02 16:00:06 -0500

    Payment reminders

--- VitalVues/Controllers/PaymentController.cs
+++ VitalVues/Controllers/PaymentController.cs
@@ -5,6 +5,9 @@ using Microsoft.AspNetCore.Identity;
 using Services.Interfaces;
 using Services.ViewModels;
 using System.Security.Claims;
+using Hangfire;
+using System;
+using Services.Services;
 
 namespace VitalVues.Controllers;
 
@@ -14,11 +17,14 @@ public class PaymentController : Controller
 {
     private readonly ILogger<UserController> _logger;
     private readonly IUserService _userService;
+    private readonly SendGridEmailService _sendGridEmailService;
+    private static Dictionary<string, string> paymentJobTracker = new Dictionary<string, string>();
 
-    public PaymentController(ILogger<UserController> logger, IUserService userService)
+    public PaymentController(ILogger<UserController> logger, IUserService userService, SendGridEmailService sendGridEmailService)
     {
         _logger = logger;
         _userService = userService;
+        _sendGridEmailService = sendGridEmailService;
     }
 
     [NoCacheHeaders]
@@ -33,12 +39,12 @@ public class PaymentController : Controller
         }
 
         var user = _userService.FindUser(userUniqueIdentifier);
-		return View(user);
+        return View(user);
     }
 
     [HttpGet("SuccessMonthly")]
-    public async Task<IActionResult> SuccessMonthly() 
-    { 
+    public async Task<IActionResult> SuccessMonthly()
+    {
         var userUniqueIdentifier = User.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")?.Value;
 
         if (string.IsNullOrEmpty(userUniqueIdentifier))
@@ -58,9 +64,26 @@ public class PaymentController : Controller
             user.SubscriptionEndDate = DateTime.UtcNow.AddMonths(1);
 
             _userService.UpdateUser(user);
+
+            var userEmail = user.Email;
+
+            // Schedule an email reminder a month before the subscription ends
+            var reminderTime = user.SubscriptionEndDate?.AddDays(-1);
+            if (reminderTime.HasValue && reminderTime.Value > DateTime.UtcNow)
+            {
+                var jobIdReminder = BackgroundJob.Schedule(() => _sendGridEmailService.SendEmail(
+                    userEmail,
+                    "Subscription Reminder",
+                    "Your subscription is about to expire",
+                    "<p>Your subscription will expire in one day.</p>" +
+                    "<p>Please renew your subscription to continue enjoying our services.</p>"
+                ), reminderTime.Value - DateTime.UtcNow);
+
+                // Store the job ID for the reminder email
+                paymentJobTracker[$"{user.Id}_subscriptionReminder"] = jobIdReminder;
+            }
         }
 
         return View("SuccessMonthly", user);
     }
 }
-
